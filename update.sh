79fcd52ce7185677f0c62449da153e65f728daa6
Miquel Massot <miquel.massot@uib.cat>
2016-04-21 11:07:13 +0200

    update

--- update.sh
+++ update.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-git submodule foreach git pull
+#git submodule foreach git pull
+
+find_git_branch() {
+  # Based on: http://stackoverflow.com/a/13003854/170413
+  local branch
+  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
+    if [[ "$branch" == "HEAD" ]]; then
+      branch='detached*'
+    fi
+    git_branch="($branch)"
+  else
+    git_branch=""
+  fi
+}
+
+#!/bin/bash
+for d in */;
+{ 
+  cd "$d"; 
+  echo "********* $d ***********"; 
+  git pull origin $find_git_branch; 
+  cd ..; 
+}
+
